16d97f7b4e672b6d448ec9d6cfbd7c49b1635a57
Big Smarty <77147643+Big-Smarty@users.noreply.github.com>
2022-02-09 19:57:06 +0100

    Added proper debugging (was very necessary)
    Added a minor amount of debugging. Nearly had to gouge out my eyes as I saw this debug-less code. Disgusting.

--- sort.py
+++ sort.py
@@ -1,6 +1,12 @@
 import os
+print("Hello SortPy (on line 2)")
+print("Second 'Hello Sortpy' (on line 3)")
+print("Importing datetime from datetime (line 4)")
 from datetime import datetime
+print("Imported datetime(line 6)")
 day = int(datetime.today().strftime('%d'))
+print("Getting today's date (line 8)")
+print("Finding out which week of the month it is (line 9)")
 if day > 7:
     if day > 14:
         if day > 21:
@@ -11,8 +17,10 @@ if day > 7:
         week = "2"
 else:
     week = "1"
-
+print("Found out which week of the month it is (line 20)")
+print("Creating a new string that holds information of today's year and month (line 21)")
 string = str(datetime.today().strftime('%Y-%m'))
+print("Created a new string that holds information about today's year and month (line 23)")
 F = string + '-' + week
 try:
     os.makedirs(F)
